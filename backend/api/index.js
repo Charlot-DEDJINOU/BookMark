4032ffd13939db9c98cf56ffa48ca93c143c359c
Charlot DEDJINOU <dedjinoucharlotjoel@gmail.com>
2025-08-20 12:51:02 +0100

    api directory

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,5 +1,3 @@
 const app = require("../dist/app").default; // importe ton app Express compilée
 
-module.exports = (req, res) => {
-  return app(req, res); // Vercel attend une fonction (req,res)
-};
+module.exports = app
